8c9f3bcc0fb084645997d4c8fa5cf5c779cac2db
Mari <mari@dhcp-10-134-216-218.uawifi.arizona.edu>
2018-09-12 14:09:36 -0700

    Table view working

---
+++ TumblrFeed/TumblrFeed/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Mari on 9/12/18.
+//  Copyright © 2018 Mari. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
